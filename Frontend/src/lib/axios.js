c28d07c5b18463639443a7db135a2c840dc51bf4
Bright846 <adarsh27102003@gmail.com>
2025-06-19 15:40:52 +0530

    Updated Some Code

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "DEVELOPMENT" ? 'http://localhost:1007' : "/",
+    baseURL: import.meta.env.MODE === "DEVELOPMENT" ? 'http://localhost:1007' : "/*",
     withCredentials: true
 });
